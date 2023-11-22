75e09d1f51368ce4f8037ccb073113ec996e26a4
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-11-22 14:27:28 -0400

    Creados formularios de login y signup en el front y enlazados a las rutas del front respectivas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,23 @@
-
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Home} from './pages/home.jsx';
+import { Login } from './pages/login.jsx';
+import { SignUp } from './pages/signup.jsx';
 
 function App() {
 
+  // const basename = process.env.FRONTEND_URL || "";
+
   return (
     <>
-      <div className='bg-purple-500'>App.js</div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home/>} path='/'/>
+          <Route element={<Login />} path="/login" />
+          <Route element={<SignUp />} path="/signup" />
+        </Routes>
+      
+      </BrowserRouter>
     </>
   )
 }
---
+++ front/src/pages/home.jsx
@@ -0,0 +1,7 @@
+export const Home = () =>{
+    return(
+        <>
+            <div className='bg-purple-500'>Home.js</div>
+        </>
+    )
+}
