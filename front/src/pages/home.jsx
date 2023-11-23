dafb570b5807b6322dc9d9418e1909b267e97fe2
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-11-22 20:55:05 -0400

    Agregado mensaje inicial en home cuando el usuario no este logueado

--- front/src/pages/home.jsx
+++ front/src/pages/home.jsx
@@ -1,7 +1,15 @@
 export const Home = () =>{
     return(
         <>
-            <div className='bg-purple-500'>Home.js</div>
+            <div className="h-screen flex justify-center items-center">
+                <div role="alert" className="alert m-3 h-24 flex justify-center text-white bg-rose-600 transition duration-0 hover:duration-75 shadow-2xl">
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6 border-white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> */}
+                    <div className="flex flex-col">
+                        <span>Alerta! Aún No has iniciciado sesión.</span>
+                        <span>Por favor inicia sesión o registrate ahora para comenzar a disfrutar del Digimundo</span>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
