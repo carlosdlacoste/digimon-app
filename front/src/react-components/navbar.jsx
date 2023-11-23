2cf7789f074dfbfd28cb58a1f95a56833e33dcb2
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-11-22 21:05:21 -0400

    Redireccionando a las rutas login y signup luego de clickear en los botones respectivos usando el componente Link de react-router-dom

--- front/src/react-components/navbar.jsx
+++ front/src/react-components/navbar.jsx
@@ -1,14 +1,21 @@
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
     return(
         <>
             <div className="navbar bg-amber-400">
                 <div className="navbar-start">
-                    <span className="p-2 ml-2 text-xl shadow-xl bg-red-600 rounded-full rotate-12">DigiDex</span>
+                    <Link to="/">
+                        <span className="p-2 ml-2 text-xl shadow-xl bg-red-600 rounded-full rotate-12">DigiDex</span>
+                    </Link>
                 </div>
             <div className="navbar-end gap-x-3">
-                <button className="btn bg-emerald-500">Log In</button>
-                <button className="btn border-emerald-500">Sign Up</button>
+                <Link to="/login">
+                    <button className="btn bg-emerald-500">Log In</button>
+                </Link>
+                <Link to="/signup">
+                    <button className="btn border-emerald-500">Sign Up</button>
+                </Link>
             </div>
             </div>
         </>
