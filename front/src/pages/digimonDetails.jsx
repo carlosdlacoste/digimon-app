e530b5592e14a280f28afe9fd5c2375e92a16cdb
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-01-22 18:46:33 -0400

    Agregado spinner de carga en la pagina digimonDetails

--- front/src/pages/digimonDetails.jsx
+++ front/src/pages/digimonDetails.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CircleLoader from "react-spinners/CircleLoader";
 
 
 export const DigimonDetails = () => {
     const [digimonDetails, setDigimonDetails] = useState({ images: [], levels: [], types: [], attributes: [], fields: [] })
+    const [loading, setLoading] = useState(true)
     const {id} = useParams()
 
     async function getDigimonDetail(id) {
@@ -21,75 +23,88 @@ export const DigimonDetails = () => {
 
     useEffect(() => {
         getDigimonDetail(id)
+        setLoading(true)
+        setTimeout(() =>{
+            setLoading(false)
+        }, 2000)
     }, []);
 
     return(
         <>
 
             <div className='h-screen flex justify-center items-center'>
-                <div className="card card-side  w-3/6 bg-base-100 shadow-xl">
-                    {
-                        digimonDetails.images.map((image, index) => (
-
-                            <figure key={index}><img src={image.href} alt="Algun Digimon" className='object-cover w-full'/></figure>
-                        ))
-                    }
-                    <div className="card-body p-2 h-96 gap-y-11">
-                        <h2 className="card-title justify-center text-3xl border-2 border-rose-700">{digimonDetails.name}</h2>
-                        <div className='flex flex-row items-center bg-emerald-600 shadow-lg text-white font-bold'>
-
-                            <p>Level:</p>
+                {
+                    loading ? 
+                    <>
+                        <CircleLoader color="#36d7b7" loading={loading} size={120} />
+                    </>
+                    :
+                    <>
+                        <div className="card card-side  w-3/6 bg-base-100 shadow-xl">
                             {
-                                digimonDetails.levels.length !== 0 ? digimonDetails.levels.map((level, index) => (
+                                digimonDetails.images.map((image, index) => (
 
-                                    <p key={index} className='text-xs'>{level.level}</p>
+                                    <figure key={index}><img src={image.href} alt="Algun Digimon" className='object-cover w-full'/></figure>
                                 ))
-
-                                :
-                                    <p className='text-xs text-red-300'>Classified</p> 
                             }
+                            <div className="card-body p-2 h-96 gap-y-11">
+                                <h2 className="card-title justify-center text-3xl border-2 border-rose-700">{digimonDetails.name}</h2>
+                                <div className='flex flex-row items-center bg-emerald-600 shadow-lg text-white font-bold'>
+
+                                    <p>Level:</p>
+                                    {
+                                        digimonDetails.levels.length !== 0 ? digimonDetails.levels.map((level, index) => (
+
+                                            <p key={index} className='text-xs'>{level.level}</p>
+                                        ))
+
+                                        :
+                                            <p className='text-xs text-red-300'>Classified</p> 
+                                    }
+                                </div>
+                                <div className='flex flex-row items-center bg-emerald-600 shadow-lg text-white font-bold'>
+
+                                    <p>Type:</p>
+                                    {
+                                        digimonDetails.types.length !== 0 ?  digimonDetails.types.map((type, index) => (
+
+                                            <p key={index} className='text-xs'>{type.type}</p>
+                                        ))
+
+                                        :
+                                            <p className='text-xs text-red-300'>Classified</p>
+                                    }
+                                </div>
+                                <div className='flex flex-row items-center bg-emerald-600 shadow-lg text-white font-bold'>
+
+                                    <p>Atribute:</p>
+                                    {
+                                        digimonDetails.attributes.length !== 0 ?  digimonDetails.attributes.map((attribute, index) => (
+
+                                            <p key={index} className='text-xs'>{attribute.attribute}</p>
+                                        ))
+
+                                        :
+                                            <p className='text-xs text-red-300'>Classified</p>
+                                    }
+                                </div>  
+                                <div className='flex flex-row items-center bg-emerald-600 shadow-lg text-white font-bold'>
+
+                                    <p>Fields:</p>
+                                    {
+                                        digimonDetails.fields.length !== 0 ? digimonDetails.fields.map((field, index) => (
+
+                                            <p key={index} className='text-xs'>{field.field}</p>
+                                        ))
+
+                                        :
+                                            <p className='text-xs text-red-300'>Classified</p>
+                                    }
+                                </div>  
+                            </div>
                         </div>
-                        <div className='flex flex-row items-center bg-emerald-600 shadow-lg text-white font-bold'>
-
-                            <p>Type:</p>
-                            {
-                                digimonDetails.types.length !== 0 ?  digimonDetails.types.map((type, index) => (
-
-                                    <p key={index} className='text-xs'>{type.type}</p>
-                                ))
-
-                                :
-                                    <p className='text-xs text-red-300'>Classified</p>
-                            }
-                        </div>
-                        <div className='flex flex-row items-center bg-emerald-600 shadow-lg text-white font-bold'>
-
-                            <p>Atribute:</p>
-                            {
-                                digimonDetails.attributes.length !== 0 ?  digimonDetails.attributes.map((attribute, index) => (
-
-                                    <p key={index} className='text-xs'>{attribute.attribute}</p>
-                                ))
-
-                                :
-                                    <p className='text-xs text-red-300'>Classified</p>
-                            }
-                        </div>  
-                        <div className='flex flex-row items-center bg-emerald-600 shadow-lg text-white font-bold'>
-
-                            <p>Fields:</p>
-                            {
-                                digimonDetails.fields.length !== 0 ? digimonDetails.fields.map((field, index) => (
-
-                                    <p key={index} className='text-xs'>{field.field}</p>
-                                ))
-
-                                :
-                                    <p className='text-xs text-red-300'>Classified</p>
-                            }
-                        </div>  
-                    </div>
-                </div>
+                    </>
+                }
             </div>
 
             
