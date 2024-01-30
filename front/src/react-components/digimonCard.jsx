00122a1516387a9e22794092e09fb7c197135b8f
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-01-30 19:08:29 -0400

    Actualizado mensaje de error para imagenes de digimon no disponible

--- front/src/react-components/digimonCard.jsx
+++ front/src/react-components/digimonCard.jsx
@@ -8,7 +8,7 @@ export const DigimonCard = (props) =>{
     return(
         <>
                     <div className="card w-full bg-base-100 shadow-xl hover:bg-emerald-400" onClick={() => navigate(`/digimon/${props.id}`)}>
-                        <figure><img src={props.image} alt="Algun Digimon" className='h-52 object-cover w-full' /></figure>
+                        <figure><img src={props.image} alt="Digimon not available" className='h-52 object-cover w-full' /></figure>
                         <div className="card-body">
                             <h2 className="card-title truncate justify-center">{props.name}</h2> {/* Revisar como truncar el texto adecuadamente para que se corte de una mejor forma */}
                         </div>
